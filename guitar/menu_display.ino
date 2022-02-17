d09432936a34f09ae99fad408925d77ad7421a34
ArthurHagen <arthurhagengeschaeftlich@gmail.com>
2022-02-17 09:18:12 +0100

    menu update

--- guitar/menu_display.ino
+++ guitar/menu_display.ino
@@ -1,10 +1,24 @@
-buttonStart = digitalRead(PIN);
-buttonLeft = digitalRead(PIN);
-buttonRight = digitalRead(PIN);
+buttonStart = digitalRead(8);
+buttonLeft = digitalRead(9);
+buttonRight = digitalRead(10);
 
 int menuItem = 0
-int menuItem_scroll_left[] = {};
-int menuItem_scroll_right[] = {7, };
+int menuItem_scroll_left[] = {5, 5, 5, 5};
+int menuItem_scroll_right[] = {7, 5, 5, 5};
+int menuItem_scroll_speed = 50;;
+int menuItem_count = 4
+
+byte arrow_down[8] = {
+  B00000,
+  B00100,
+  B00100,
+  B00100,
+  B00100,
+  B11111,
+  B01110,
+  B00100,
+};
+lcd.createChar(8, arrow_down);
 
 void initMenu() { //setup function
   lcd.clear();
@@ -14,8 +28,10 @@ void initMenu() { //setup function
   lcd.print("press START")
   while(buttonStart == LOW){}
   lcd.clear();
-  lcd.print("use START to select")
-  lcd.setCursor(0,1); //center first item!
+  lcd.print("use START to select");
+  lcd.setCursor(8,1); //center first row
+  lcd.write(8);
+  lcd.setCursor(0,1); //center second row
 }
 
 void loopMenu() { // loopFunction
@@ -24,17 +40,22 @@ void loopMenu() { // loopFunction
     menuItem--;
     
     if(menuItem < 0){
-      menuItem = 4;
-      lcd.scrollDisplayLeft(menuItem_scroll_left[menuItem]);
+      menuItem = menuItem_count;
+      for(j=0; j < menuItem_scroll_left[menuItem]; j++){
+        lcd.scrollDisplayLeft();
+        delay(menuItem_scroll_speed);
     }
   }
 
   if(buttonRight == HIGH){
     menuItem++;
     
-    if(menuItem > 4){
+    if(menuItem > menuItem_count){
       menuItem = 0;
-      lcd.scrollDisplayRight(menuItem_scroll_right[menuItem]);
+      for(j=0; j < menuItem_scroll_right[menuItem]; j++){
+        lcd.scrollDisplayRight();
+        delay(menuItem_scroll_speed);
+      }
     }
   }
 }
