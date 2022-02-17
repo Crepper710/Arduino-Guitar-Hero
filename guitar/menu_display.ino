e60e76ffbfd41fcf8f1213cd3d0fd01d7eae0156
ArthurHagen <arthurhagengeschaeftlich@gmail.com>
2022-02-17 13:54:41 +0100

    menu neu

--- guitar/menu_display.ino
+++ guitar/menu_display.ino
@@ -1,32 +1,21 @@
-buttonStart = digitalRead(8);
-buttonLeft = digitalRead(9);
-buttonRight = digitalRead(10);
-
-int menuItem = 0
+int menuItem = 0;
 int menuItem_scroll_left[] = {5, 5, 5, 5};
 int menuItem_scroll_right[] = {7, 5, 5, 5};
-int menuItem_scroll_speed = 50;;
-int menuItem_count = 4
+int menuItem_scroll_speed = 50;
+int menuItem_count = 4;
 
-byte arrow_down[8] = {
-  B00000,
-  B00100,
-  B00100,
-  B00100,
-  B00100,
-  B11111,
-  B01110,
-  B00100,
-};
-lcd.createChar(8, arrow_down);
+
+extern bool functionButtonPressed();
+extern bool lane2ButtonPressed();
+extern bool lane1ButtonPressed();
 
 void initMenu() { //setup function
   lcd.clear();
   lcd.autoscroll();
   lcd.print("Guitar Dude");
   lcd.setCursor(0,1);
-  lcd.print("press START")
-  while(buttonStart == LOW){}
+  lcd.print("press START");
+  while(functionButtonPressed() == LOW){}
   lcd.clear();
   lcd.print("use START to select");
   lcd.setCursor(8,1); //center first row
@@ -35,24 +24,24 @@ void initMenu() { //setup function
 }
 
 void loopMenu() { // loopFunction
-  lcd.print("[play]    [level selection]    [difficulty]    [creddits]    ") //4 items
-  if(buttonLeft == HIGH){
+  lcd.print("[play]    [level selection]    [difficulty]    [creddits]    "); //4 items
+  if(lane2ButtonPressed() == HIGH){
     menuItem--;
     
     if(menuItem < 0){
       menuItem = menuItem_count;
-      for(j=0; j < menuItem_scroll_left[menuItem]; j++){
+      for(int i=0; i < menuItem_scroll_left[menuItem]; i++){
         lcd.scrollDisplayLeft();
         delay(menuItem_scroll_speed);
     }
   }
 
-  if(buttonRight == HIGH){
+  if(lane1ButtonPressed() == HIGH){
     menuItem++;
     
     if(menuItem > menuItem_count){
       menuItem = 0;
-      for(j=0; j < menuItem_scroll_right[menuItem]; j++){
+      for(int j=0; j < menuItem_scroll_right[menuItem]; j++){
         lcd.scrollDisplayRight();
         delay(menuItem_scroll_speed);
       }
