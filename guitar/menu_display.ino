ddae4a493febbb01457f776ca5491e574d157abe
ArthurHagen <arthurhagengeschaeftlich@gmail.com>
2022-03-20 16:02:49 +0100

    added comments

--- guitar/menu_display.ino
+++ guitar/menu_display.ino
@@ -7,12 +7,14 @@ extern bool joyStickRight();
 extern bool joyStickUp();
 extern bool joyStickDown();
 
+//settings for main menu
 String menuItem_display[] = {"[play]","[level]"};
-int menuItem_display_center[] = {5, 5};
-int menuItem = 0;
-int menuItem_scroll_speed = 50;
-int menuItem_count = 1;
+int menuItem_display_center[] = {5, 5}; //needed spaces for displaying in center
+int menuItem = 0; //selected item
+int menuItem_count = 1; //amount of items
 
+//settings for level selection
+//items are fetched from sound.ino
 int levelItem_display_center[] = {5, 5};
 int levelItem = 0;
 int levelItem_count = getTrackCount();
@@ -23,10 +25,9 @@ int cursor_delay = 300;
 int menuSection = 0; //0=menu; 1=level;
 
 
-
 void initMenu() { //setup function
   gameStatus = false;
-  if (firstStart == true){
+  if (firstStart == true){ //initial starting screen is only shown on boot
     lcd.clear();
     lcd.print("Guitar Dude");
     lcd.setCursor(0,5);
@@ -35,23 +36,18 @@ void initMenu() { //setup function
   }
     delay(1000);
     firstStart = false;
-    //lcd.clear();
-    //lcd.print("use START to select");
     lcd.clear();
     lcd.setCursor(8,0); //center first row
     lcd.write(7);
-    //lcd.setCursor(5,1); //center second row
-    //lcd.print(menuItem_display[0]);
 }
 
 void loopMenu() { // loopFunction
-  switch(menuSection){
+  switch(menuSection){ //includes main menus & level selection
     case 0: //menu
       lcd.setCursor(8,0); //center first row
       lcd.write(7);
       lcd.setCursor(menuItem_display_center[menuItem], 1);
       lcd.print(menuItem_display[menuItem]);
-      //delay(cursor_delay);
     
       if(joyStickDown()){
         menuItem--;
@@ -59,11 +55,11 @@ void loopMenu() { // loopFunction
         if(menuItem < 0){
           menuItem = menuItem_count;
         }
-        lcd.clear();
-        lcd.setCursor(8,0); //center first row
+      lcd.clear();
+      lcd.setCursor(8,0); //center first row
       lcd.write(7);
-      lcd.setCursor(menuItem_display_center[menuItem], 1);
-      lcd.print(menuItem_display[menuItem]);
+      lcd.setCursor(menuItem_display_center[menuItem], 1);  //ajusts cursor to center
+      lcd.print(menuItem_display[menuItem]);  //writes current menu item
       delay(cursor_delay);
       }
     
@@ -75,11 +71,12 @@ void loopMenu() { // loopFunction
           lcd.clear();
           lcd.setCursor(8,0); //center first row
       lcd.write(7);
-      lcd.setCursor(menuItem_display_center[menuItem], 1);
-      lcd.print(menuItem_display[menuItem]);
+      lcd.setCursor(menuItem_display_center[menuItem], 1);  //ajusts cursor to center
+      lcd.print(menuItem_display[menuItem]);  //writes current menu item
       delay(cursor_delay);
       }
-    
+
+    //navigates to selected menu/function
       if(functionButtonPressed()){
         switch(menuItem){
           case 0: //play
@@ -105,7 +102,7 @@ void loopMenu() { // loopFunction
         lcd.print("[return]");
       } else {
         lcd.setCursor(0, 1);
-        lcd.print(getTrackName(levelItem - 1));
+        lcd.print(getTrackName(levelItem - 1)); //fetches the level name from sound.ino
       }
       
       if(joyStickDown()){
@@ -122,7 +119,7 @@ void loopMenu() { // loopFunction
         lcd.print("[return]");
       } else {
         lcd.setCursor(0, 1);
-        lcd.print(getTrackName(levelItem - 1));
+        lcd.print(getTrackName(levelItem - 1)); //fetches the level name from sound.ino
       }
       delay(cursor_delay);
       }
@@ -138,15 +135,15 @@ void loopMenu() { // loopFunction
       if(levelItem == 0){
         lcd.setCursor(4, 1);
         lcd.print("[return]");
-      } else {
-        lcd.setCursor(0, 1);
-        lcd.print(getTrackName(levelItem - 1));
       }
-      //lcd.setCursor(levelItem_display_center[levelItem], 1);
-      //lcd.print(levelItem_display[levelItem]);
+      else {
+        lcd.setCursor(0, 1);
+        lcd.print(getTrackName(levelItem - 1)); //fetches the level name from sound.ino
+      }
       delay(cursor_delay);
       }
-    
+
+    //navigates to selected menu/function
       if(functionButtonPressed()){ 
         if(levelItem != 0){
           setCurrentTrack(levelItem -1);
@@ -163,6 +160,6 @@ void loopMenu() { // loopFunction
   
 }
 
-bool startGame() {
+bool startGame() { //starts game if true
   return gameStatus;
 }
